44a8beac87ed001cf06d0b7bd090d6ea833c85a6
Анастасия <ll-anastasiia.medvedeva-ll@yandex.ru>
2019-11-17 19:17:55 +0300

    Updates

---
+++ precache-manifest.4696a70303556797d16879affc33a9b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c087d4352a142067a9686012eb7ba57",
+    "url": "/React_diploma_Store/static/media/footer-sprite.9c087d43.png"
+  },
+  {
+    "revision": "d613d452bebe22d8b7d737ec12001e04",
+    "url": "/React_diploma_Store/static/media/banner.d613d452.jpg"
+  },
+  {
+    "revision": "f4f5c5861adce12db65c",
+    "url": "/React_diploma_Store/static/js/runtime~main.66611046.js"
+  },
+  {
+    "revision": "26ce71dfd0cc95c9b551",
+    "url": "/React_diploma_Store/static/js/main.962db053.chunk.js"
+  },
+  {
+    "revision": "86deb5320a0deb9af0d9",
+    "url": "/React_diploma_Store/static/js/2.7eb1b3a1.chunk.js"
+  },
+  {
+    "revision": "26ce71dfd0cc95c9b551",
+    "url": "/React_diploma_Store/static/css/main.e3a0def0.chunk.css"
+  },
+  {
+    "revision": "cb1a258af3ef72c0ca8ed2de61510077",
+    "url": "/React_diploma_Store/index.html"
+  }
+];
